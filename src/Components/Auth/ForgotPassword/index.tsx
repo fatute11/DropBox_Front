0db20dc36b22d38fc97b78ab9479b02a62f42258
mehdi <mehdimedjdoub@gmail.com>
2020-12-18 12:07:05 +0100

    add password validation

--- src/Components/Auth/ForgotPassword/index.tsx
+++ src/Components/Auth/ForgotPassword/index.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import API from '../../../Api/Api'
 import { Grid, FormControl, InputLabel, Input, FormHelperText, Button } from '@material-ui/core';
 
-interface S{ email: string }
+interface S{ email: string, isSend: boolean, notFound: boolean }
 
 export default class ForgotPassword extends React.PureComponent<{}, S>{
 
-    state: Readonly<S> = { email: "" };
+    state: Readonly<S> = { email: "", isSend: false, notFound: false };
 
-    handleSubmit = () => {        
-        API.post('/forgot-password', this.state).then(res => {
-            console.log(res);
-        })
+    handleSubmit = async () => {        
+        const data = await API.post('/forgot-password', this.state).then(res => {return res.data})
+        console.log(data)
+        if(data.error) {
+            this.setState({notFound: true})
+        }else{
+            this.setState({isSend: true})
+        }
     }
 
     handleEmailChange= (e) => {
@@ -20,19 +24,30 @@ export default class ForgotPassword extends React.PureComponent<{}, S>{
     }
 
     render() {
-        const { email } = this.state;
+        const { email, isSend, notFound } = this.state;
 
-        return(
-            <Grid>
-                <form>
-                    <FormControl>
-                        <InputLabel htmlFor="forgot-password">Email address</InputLabel>
-                        <Input id="forgot-password" aria-describedby="forgot-password" value={email} onChange={e => this.handleEmailChange(e)}/>
-                        <FormHelperText id="forgot-password">Enter your Email to reset your password.</FormHelperText>
-                    </FormControl>
-                    <Button variant="contained" color="primary" size="medium" onClick={e => this.handleSubmit()}>Reset Password</Button>
-                </form>
-            </Grid>
-        )
+        if(isSend) {
+            return(
+                <Grid>
+                    <p>un email viens d'être envoyé a {email}</p>
+                </Grid>
+            )
+        }else{
+            return(
+                <Grid>
+                    <form>
+                        <FormControl>
+                            <InputLabel htmlFor="forgot-password">Email address</InputLabel>
+                            <Input id="forgot-password" aria-describedby="forgot-password" value={email} onChange={e => this.handleEmailChange(e)}/>
+                            <FormHelperText id="forgot-password">Enter your Email to reset your password.</FormHelperText>
+                            {notFound &&
+                                <p>Adresse mail non valide</p>
+                            }
+                        </FormControl>
+                        <Button variant="contained" color="primary" size="medium" onClick={e => this.handleSubmit()}>Reset Password</Button>
+                    </form>
+                </Grid>
+            )
+        }
     }
 }
--- src/Components/Auth/ResetPassword/index.tsx
+++ src/Components/Auth/ResetPassword/index.tsx
@@ -2,32 +2,57 @@ import React from 'react';
 import API from '../../../Api/Api'
 import { Grid, FormControl, InputLabel, Input, FormHelperText, Button } from '@material-ui/core';
 
-interface S{ password: string, passwordAgain: string, isTokenValid:boolean  }
+interface S{ 
+    password: string, 
+    passwordAgain: string, 
+    isTokenValid: boolean, 
+    userId: string  
+}
 
 export default class ResetPassword extends React.PureComponent<{}, S>{
 
-    state: Readonly<S> = { password: "", passwordAgain: "", isTokenValid: false };
+    state: Readonly<S> = { 
+        password: "", 
+        passwordAgain: "", 
+        isTokenValid: false, 
+        userId: "" 
+    };
 
     componentDidMount = async () => {
         const arrPath = window.location.pathname.split("/")
         const token = arrPath[arrPath.length - 1]
 
-        const data = await API.get('/api/user/password-token', {params: {passwordToken: token}}).then(response => {return response.data})
+        const data = await API
+        .get('/api/user/password-token', {params: {passwordToken: token}})
+        .then(response => {return response.data})
         if(!data.error) {
-            this.setState({ isTokenValid:true })
+            this.setState({ isTokenValid:true, userId: data._id })
         }
     }
     
     handleSubmit = () => { 
-        console.log(this.state)       
-        // API.post('/reset-password', this.state).then(res => {
-        //     console.log(res);
-        // })
+
+        const response = this.passwordValidation(this.state.password, this.state.passwordAgain)   
+        console.log(response)
+        if (response.error) {
+
+        } 
+        API.post('/reset-password', {password: response, id: this.state.userId}).then(res => {console.log(res);})
     }
 
-    handlePasswordChange= (e) => {
+    handlePasswordChange = (e) => {
         console.log(e.target)
         this.setState({ [e.target.name]: e.target.value } as S)
+    }
+
+    passwordValidation = (password, passwordAgain) => {
+        if(password != passwordAgain) {
+            return {
+                error:'not same password', 
+                message: 'Les mots de passe ne sont pas identiques !'
+            }
+        }
+        return password
     }
 
     render() {

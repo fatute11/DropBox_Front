56522960748f3761f0527043131c04201bc3e633
mehdi <mehdimedjdoub@gmail.com>
2020-12-21 22:41:58 +0100

    feature/userlist: display list of user files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, HashRouter, Route, Switch } from "react-router-dom";
 import ForgotPassword from "./Layouts/ForgotPassword"
 import ResetPassword from "./Layouts/ResetPassword"
+import FileList from "./Layouts/UserFiles/index"
+import UploadFiles from "./Components/UploadFolder"
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
                 <Switch>
                     <Route exact path="/forgot-password/" component={ForgotPassword}></Route>
                     <Route exact path="/reset-password/:token/" component={ResetPassword}></Route>
+                    <Route exact path="/file-list" component={FileList}></Route>
+                    <Route exact path="/upload-file" component={UploadFiles}></Route>
                 </Switch>
             </Router>
         </div>
---
+++ src/Components/FileList/styles.tsx
@@ -0,0 +1,10 @@
+import { createStyles, Theme } from "@material-ui/core";
+
+export type Styles = "iconFile";
+const styles = (theme: Theme) => createStyles<Styles, {}>({
+    iconFile:{
+
+    }
+});
+
+export default styles;

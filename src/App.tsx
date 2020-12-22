f54c53349db97c62cd179b6f7db38bee3a8f8a35
mehdi <mehdimedjdoub@gmail.com>
2020-12-22 12:10:21 +0100

    add authentications routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,29 @@ import ResetPassword from "./Layouts/ResetPassword"
 import FileList from "./Layouts/UserFiles/index"
 import UploadFiles from "./Components/UploadFolder"
 
+import HeaderBar from "./Components/HeaderBar"
+
+import Login from "./Components/login.component";
+import Register from "./Components/register.component";
+import Home from "./Components/home.component";
+import Profile from "./Components/profile.component";
+import BoardUser from "./Components/board-user.component";
+// import BoardModerator from "./Components/board-moderator.component";
+// import BoardAdmin from "./Components/board-admin.component";
+
 function App() {
     return (
         <div>
             <Router>
+                <HeaderBar />
                 <Switch>
+                    <Route exact path='[/, /home]' component={Home} isAuthed={true}/>
+                    <Route exact path='/login' component={Login} isAuthed={true}/>
+                    <Route exact path='/register' component={Register} isAuthed={true} />
+                    <Route exact path='/profile' component={Profile} isAuthed={true} />
+                    <Route path='/user' component={BoardUser} isAuthed={true} />
+                    {/* <Route path='/mod' component={BoardModerator} isAuthed={true} />
+                    <Route path='/admin' component={BoardAdmin} isAuthed={true} /> */}
                     <Route exact path="/forgot-password/" component={ForgotPassword}></Route>
                     <Route exact path="/reset-password/:token/" component={ResetPassword}></Route>
                     <Route exact path="/file-list" component={FileList}></Route>
